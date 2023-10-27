b54bead085fafdab6088c38e46fc696a734a9e53
AntonovAtanas <atanas.antonov1@gmail.com>
2023-10-27 16:36:42 +0300

    Add and stylize sorting button

--- src/components/Items-List/ItemsList.tsx
+++ src/components/Items-List/ItemsList.tsx
@@ -4,26 +4,36 @@ import CurrentItem from "../Item/Item";
 import "./itemsList.css";
 
 export default function ItemsList({
-    items,
-    onDelete,
-    onChecked,
+  items,
+  onDelete,
+  onChecked,
 }: {
-    items: Item[];
-    onDelete: OnDelete;
-    onChecked: OnChecked;
+  items: Item[];
+  onDelete: OnDelete;
+  onChecked: OnChecked;
 }) {
-    return (
-        <div className="list">
-            <ul>
-                {items.map((item: Item) => (
-                    <CurrentItem
-                        item={item}
-                        key={item.id}
-                        onDelete={onDelete}
-                        onChecked={onChecked}
-                    />
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <>
+      <div className="list">
+        <ul>
+          {items.map((item: Item) => (
+            <CurrentItem
+              item={item}
+              key={item.id}
+              onDelete={onDelete}
+              onChecked={onChecked}
+            />
+          ))}
+        </ul>
+      </div>
+      <div className="actions">
+        <select>
+          <option value="input">Sort by input order</option>
+          <option value="alphabetical">Sort by name</option>
+          <option value="packed">Sorted by packed status</option>
+        </select>
+        {/* <button></button> */}
+      </div>
+    </>
+  );
 }
